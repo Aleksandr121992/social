bed58afbebf262a4b17806436a72de707fd902cc
Aleksandr121992 <alik.muradyan92@gmail.com>
2019-05-25 15:55:58 +0400

    new

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+<div class="row">
+          <div class="col-md-2 col-sm-6 portfolio-item" style="border:2px solid #675F5F">
+            aaaaa
+          </div>
+          <div class="col-md-7 col-sm-6 portfolio-item" style="border:2px solid #675F5F">
+            bbbb
+          </div>
+          <div class="col-md-3 col-sm-6 portfolio-item" style="border:2px solid #675F5F">
+            ccc
+          </div>
+</div>          
+@endsection
